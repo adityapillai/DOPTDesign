8afb2fe4500edc2aa2cd1bd8d1a674ad293aa424
Aditya Pillai <aditya.pillai53916@gmail.com>
2024-01-23 10:54:49 -0500

    made general local search function to simplify local search code, cleaned up formatting

--- localZ.py
+++ localZ.py
@@ -1,16 +1,34 @@
 import utilZ
 import numpy as np
 import time
-import math
-import sys
+
 
 def remove_Matrix(S, i):
-    M = np.delete(S, i, axis = 1)
+    M = np.delete(S, i, axis=1)
     return M @ M.T
 
 
-def localMove_pairs(S, Pairs, oldValue):
+def best_starting_sol(arr_s):
+    prods = arr_s @ np.transpose(arr_s, axes=(0, 2, 1))
+    values = np.linalg.det(prods)
+    max_idx = np.argmax(values)
+    return values[max_idx], max_idx
 
+
+def general_local_alg(S, current_val, local_move_fun):
+    iterations = 0
+    while True:
+        new_val, replace_index, new_vec = local_move_fun(S)
+        iterations += 1
+        if new_val - current_val < 10 ** (-5):
+            break
+        current_val = new_val
+        S[:, replace_index] = new_vec
+
+    return current_val, iterations
+
+
+def local_move_pairs(S, Pairs):
     d, k = S.shape
     # starting point
     pm = utilZ.pairsMat(S, Pairs)
@@ -18,204 +36,170 @@ def localMove_pairs(S, Pairs, oldValue):
     dS = np.linalg.det(pm @ pm.T)
     Sinv = np.linalg.inv(pm @ pm.T)
 
-    #print(S)
-    replace_index = 0
-    addVector = np.zeros(d)
-    maxVal = oldValue
+    # print(S)
+    replace_index, addVector, maxVal = 0, 0, 0
     for i in range(k):
         tmp = remove_Matrix(S, i)
-        pmi = utilZ.pairsMat(tmp)
+        pmi = utilZ.pairsMat(tmp, pairs=Pairs)
 
         Ri = pmi @ pmi.T
         dRi = np.linalg.det(Ri)
 
-        if dRi < 10**(-15) or d == k:
+        if dRi < 10 ** (-15) or d == k:
             dRi = 0
 
         c = pm[:, i]
         tmp = np.multiply(c, c.reshape(-1, 1))
-        QM = Sinv + (Sinv @ tmp @ Sinv)/(1 - c @ Sinv @ c) if dRi > 0 else -1 * np.linalg.pinv(Ri, hermitian = True) @ Ri
+        QM = Sinv + (Sinv @ tmp @ Sinv) / (1 - c @ Sinv @ c) if dRi > 0 else -1 * np.linalg.pinv(Ri,
+                                                                                                 hermitian=True) @ Ri
 
-
-        sol = utilZ.quadIP(QM, pairs = Pairs)
+        sol = utilZ.quadIP(QM, pairs=Pairs)
 
         newSol = sol[0]
         # np.array(sol[0]).reshape((d, 1))
 
         norm2 = np.array(sol[1]) @ np.array(sol[1])
-        currentVal = dRi*(1 + newSol) if dRi > 0 else (dS/(c@c + S[:, i] @ QM @ S[:, i]))*(norm2 + newSol)
+        currentVal = dRi * (1 + newSol) if dRi > 0 else (dS / (c @ c + S[:, i] @ QM @ S[:, i])) * (norm2 + newSol)
         #
-        #currentVal = dRi*(1 + m.getObjective().getValue())
+        # currentVal = dRi*(1 + m.getObjective().getValue())
         if currentVal > maxVal:
             replace_index = i
             addVector = sol[1]
             maxVal = currentVal
 
-    return maxVal > oldValue, replace_index, addVector, maxVal
+    return maxVal, replace_index, addVector
 
 
+def local_alg_pairs(k, d, Pairs):
+    info = {"F/s": (d - 1, k), "Total Time": time.perf_counter()}
+    trials = 50
+    arr_s = np.ones((trials, d, k))
 
-def localMove(S, oldValue):
+    utilZ.random_b(arr_s)
+    arr_s_pairs = np.array([utilZ.pairsMat(S, Pairs) for S in arr_s])
+    current_val, max_idx = best_starting_sol(arr_s_pairs)
+    S = arr_s[max_idx]
 
-    d = S.shape[0]
-    k = S.shape[1]
+    def local_pairs_fun(S):
+        return local_move_pairs(S, Pairs)
+
+    current_val, iterations = general_local_alg(S, current_val, local_pairs_fun)
+
+    info["Final Value"] = np.log(current_val)
+    info["Iterations"] = iterations
+    info["Total Time"] = time.perf_counter() - info["Total Time"]
+    return info
+
+
+def local_move(S):
+    d, k = S.shape
     # starting point
-    dS = np.linalg.det(S @ S.T)
-    Sinv = np.linalg.inv(S @ S.T)
+    SS = S @ S.T
+    dS = np.linalg.det(SS)
+    Sinv = np.linalg.inv(SS)
 
-    #print(S)
-    replace_index = 0
-    addVector = np.zeros(d)
-    maxVal = oldValue
+    # print(S)
+    replace_index, add_vector, max_val = 0, 0, 0
     for i in range(k):
+        c = S[:, i].reshape((d, 1))
+        tmp = c @ c.T
 
-        Ri = remove_Matrix(S, i)
+        Ri = SS - tmp
         dRi = np.linalg.det(Ri)
 
-        if dRi < 10**(-15) or d == k:
+        if dRi < 10 ** (-15) or d == k:
             dRi = 0
 
-        c = S[:, i]
-        tmp = np.multiply(c, c.reshape(-1, 1))
-        QM = Sinv + (Sinv @ tmp @ Sinv)/(1 - c @ Sinv @ c) if dRi > 0 else -1 * np.linalg.pinv(Ri, hermitian = True) @ Ri
-
+        c = c.flatten()
+        QM = Sinv + (Sinv @ tmp @ Sinv) / (1 - c @ Sinv @ c) if dRi > 0 else -1 * np.linalg.pinv(Ri,
+                                                                                                 hermitian=True) @ Ri
 
         sol = utilZ.quadIP(QM)
-
-        newSol = sol[0]
-        # np.array(sol[0]).reshape((d, 1))
+        sol_val = sol[0]
 
         norm2 = np.array(sol[1]) @ np.array(sol[1])
-        currentVal = dRi*(1 + newSol) if dRi > 0 else (dS/(c@c + S[:, i] @ QM @ S[:, i]))*(norm2 + newSol)
-        #
-        #currentVal = dRi*(1 + m.getObjective().getValue())
-        if currentVal > maxVal:
+        current_val = dRi * (1 + sol_val) if dRi > 0 else (dS / (c @ c + S[:, i] @ QM @ S[:, i])) * (norm2 + sol_val)
+        if current_val > max_val:
             replace_index = i
-            addVector = sol[1]
-            maxVal = currentVal
+            add_vector = sol[1]
+            max_val = current_val
 
-    if maxVal > oldValue:
-        return [replace_index, addVector, maxVal]
-    #print("cannot find better sol")
-    return [-1]
+    return max_val, replace_index, add_vector
 
 
-def localAlg(k, d):
-    #S = [np.array(random_pm(d))[:, np.newaxis] for j in range(k)]
+def local_alg(k, d):
+    info = {"F/s": (d - 1, k), "Total Time": time.perf_counter()}
 
-    S = np.ones((d, k))
+    trials = 50
+    arr_s = np.ones((trials, d, k))
 
-    utilZ.random_b(S, d, k)
-    #print(S)
-    #utilZ.gramMatrix(S, d, k)
-    while abs(np.linalg.det(S @ S.T)) <= 10**(-5):
-        utilZ.random_b(S, d, k)
+    utilZ.random_b(arr_s)
+    current_val, max_idx = best_starting_sol(arr_s)
+    S = arr_s[max_idx]
 
+    current_val, iterations = general_local_alg(S, current_val, local_move)
 
-
-
-    currentVal = utilZ.solutionValue(S)
-    iterations = 0
-    start = time.perf_counter()
-    while  True:
-        p = localMove(S, currentVal)
-        iterations = iterations + 1
-
-        #print(p)
-        if len(p) == 1:
-            break
-        for j in range(d):
-            S[j][p[0]] = p[1][j]
-        new = p[-1]  #solutionValue(S, d, k)
-
-        if abs(new - currentVal) <= 10**(-5):# or iterations > 2:
-            break
-        currentVal = new
-
-    end = time.perf_counter() - start
-    return [iterations ,S, math.log(currentVal), end]
+    info["Total Time"] = time.perf_counter() - info["Total Time"]
+    info["Iterations"] = iterations
+    return info
 
 
-
-def localMove_p(S, q, oldValue):
-
-    d, k = S.shape
-
-
+def local_move_ob(S, q):
     scale = False
+    d, k = S.shape
+    SS = S @ S.T
+    dS = np.linalg.det(SS)
+    # Sinv = np.linalg.inv(SS)
 
-    GM = S @ S.T
+    SD = np.linalg.pinv(SS, hermitian=True)
 
-
-
-    #if scale:
-    #    GM = GM / d
-
-    dS = np.linalg.det(GM)
-
-
-    Sinv = np.linalg.inv(GM)
-
-
-    SD =  np.linalg.pinv(GM, hermitian = True)
-
-    replace_index = 0
-    addVector = np.zeros(d)
-    maxVal = oldValue
+    replace_index, add_vector, max_val = 0, 0, 0
     for i in range(k):
-
-        Ri = remove_Matrix(S, i)
-        #if scale:
+        c = S[:, i].reshape((d, 1))
+        tmp = c @ c.T
+        Ri = SS - tmp
+        c = c.flatten()
+        # if scale:
         #    Ri = Ri / d
-        #print(Ri)
-        #print(remove_Matrix(S, d, k, i))
+        # print(Ri)
+        # print(remove_Matrix(S, d, k, i))
         inv = True
         dRi = np.linalg.det(Ri)
 
-        #if scale:
+        # if scale:
         #    dRi
 
-        c = S[:, i]
-        tmp = np.multiply(c, c.reshape(-1, 1))
-        QM = 0
-
+        # do new few lines in a cleaner way
         try:
             QM = np.linalg.inv(Ri)
         except np.linalg.LinAlgError as err:
             dRi = 0
             inv = False
-            QM = -1 * np.linalg.pinv(Ri, hermitian = True) @ Ri
+            QM = -1 * np.linalg.pinv(Ri, hermitian=True) @ Ri
 
+        sol = utilZ.quadIP(QM, onesBound=q)
 
-        sol = utilZ.quadIP(QM, onesBound = q)
-        #print([sol[0], utilZ.quadIP_p([0]])
+        new_sol = sol[0] / d if scale else sol[0]
+        new_sol = max(0, new_sol) if inv else new_sol
 
-        newSol = sol[0]/d if scale else sol[0]
-
-
-
-
-        newSol = max(0, newSol) if inv else newSol
-
-        #if newSol < 0 and inv:
-        #    print(newSol)
+        # if new_sol < 0 and inv:
+        #    print(new_sol)
 
         d2 = (SD @ c).T @ (SD @ c)
 
-        currentVal = dRi*(1 + newSol) if inv else (dS*d2)*(q + newSol)
+        current_val = dRi * (1 + new_sol) if inv else (dS * d2) * (q + new_sol)
 
-        if currentVal > maxVal + 10**(-5):
+        if current_val > max_val + 10 ** (-5):
             replace_index = i
-            addVector = sol[1]
-            maxVal = currentVal
+            add_vector = sol[1]
+            max_val = current_val
 
-    return maxVal > oldValue, replace_index, addVector, maxVal
+    return max_val, replace_index, add_vector
 
 
-def localAlg_p(k, d, q):
+def local_alg_ob(k, d, q):
+    info = {"F/s": (d - 1, k), "Ones Bound": q, "Total Time": time.perf_counter(), "Iterations": 0}
 
-    info = {"F/s" : (d-1, k), "Ones Bound" : q, "Total Time" : time.perf_counter(), "Iterations" : 0 }
-    #print([math.log(sys.maxsize), d*math.log(d)])
     # get 50 random solutions and start with the best
     trials = 50
     arr_s = np.ones((trials, d, k))
@@ -223,57 +207,23 @@ def localAlg_p(k, d, q):
     for S in arr_s:
         utilZ.random_p(S, q)
 
-    prods = arr_s @ np.transpose(arr_s, axes = (0, 2, 1))
-    vals = np.linalg.det(prods)
-    max_idx = np.argmax(vals)
-    currentVal = vals[max_idx]
+    current_val, max_idx = best_starting_sol(arr_s)
     S = arr_s[max_idx]
 
-    #if not scale:
-    #    currentVal =  utilZ.solutionValue(S)
-    #else:
-    #    currentVal = np.linalg.det((S @ S.T)/d)
+    def local_move_ob_fun(S):
+        return local_move_ob(S, q)
 
-
-    #print("starting with " + str(math.log(currentVal) + d*math.log(d)) + "and det was" + str(d**d * np.linalg.det(GM/d)))
-    while  True:
-        success, replace_index, newVec, newVal = localMove_p(S, q, currentVal)
-        info["Iterations"] += 1
-
-        if not success:
-            break
-
-        S[:, replace_index] = newVec
-
-        currentVal = newVal
+    currentVal, iterations = general_local_alg(S, current_val, local_move_ob_fun)
 
     info["Total Time"] = time.perf_counter() - info["Total Time"]
-
-    #final =
-
-    #if scale:
-    #    final += d*math.log(d)
-
-    info["Final Value"] = math.log(currentVal)
-
-
+    info["Final Value"] = np.log(currentVal)
     return info
 
 
+if __name__ == "__main__":
+    d = 15
+    k = 2 * d
+    q = 5
 
-
-#lo = localAlg_p(8, 6, 2)
-'''
-
-for d in range(27,32):
-    F = d - 1
-    k = 2*d
-    q = d//3
-
-
-    print([F, k, q])
-    print("itrations, value, time")
-    l = localAlg_p(k, d, q)
-    print([l[0], l[2], l[3]])
-    print(" ")
-'''
+    # print(local_alg_ob(k, d, q))
+    print(local_alg(k, d))
--- utilZ.py
+++ utilZ.py
@@ -5,32 +5,33 @@ import sys
 import itertools
 import time
 
+
 def c2(p):
     return math.comb(p, 2)
 
 
 def colExist(S, newcol):
-    return np.all(newcol == S, axis = 0).any()
+    return np.all(newcol == S, axis=0).any()
 
 
 def random_p(S, p):
-    d,k = S.shape
+    d, k = S.shape
     S[1:, :] = 0
-    numOnes = np.random.randint(low = 0, high = p, size = k)
+    numOnes = np.random.randint(low=0, high=p, size=k)
     for j in range(k):
-        rows = np.random.choice(np.arange(1,d), numOnes[j], replace = False)
+        rows = np.random.choice(np.arange(1, d), numOnes[j], replace=False)
         S[rows, j] = 1
-    #print(S)
+
 
 def random_b(S):
-    S[1:S.shape[0]+1, :] = np.random.binomial(n = 1, p = 1/2, size = (S.shape[0]-1, S.shape[1]))
+    if S.ndim > 2:
+        S[:, 1:S.shape[1] + 1, :] = np.random.binomial(n=1, p=1 / 2, size=(S.shape[0], S.shape[1] - 1, S.shape[2]))
+    else:
+        S[1:S.shape[0] + 1, :] = np.random.binomial(n=1, p=1 / 2, size=(S.shape[0] - 1, S.shape[1]))
 
 
 def random_l(S):
-    S[1:S.shape[0] + 1, :] = np.random.randint(low = 0, high = 3, size = (S.shape[0]-1, S.shape[1]))
-
-def solutionValue(S):
-    return np.linalg.det(S @ S.T)
+    S[1:S.shape[0] + 1, :] = np.random.randint(low=0, high=3, size=(S.shape[0] - 1, S.shape[1]))
 
 
 def pairsMat(S, pairs):
@@ -42,7 +43,8 @@ def pairsMat(S, pairs):
 
     return np.vstack([S, newMat])
 
-def prodConstraint(m, a, b, c):
+
+def prod_constraint(m, a, b, c):
     m.addLConstr(c <= a)
     m.addLConstr(c <= b)
     m.addLConstr(c >= a + b - 1)
@@ -54,17 +56,16 @@ def quadFormPairs(pairsS, x, pairs):
     return pairsX @ pairsS @ pairsX
 
 
-
 def localMove(pairsS, x, pairs, intitialVal):
     d = len(x)
 
-    x_mat = np.tile(x.reshape((d, 1)), (1, d-1))
+    x_mat = np.tile(x.reshape((d, 1)), (1, d - 1))
     mod_I = np.identity(d)[:, 1:]
 
-    allChoices = np.concatenate([(x_mat + mod_I) % 3, (x_mat + 2*mod_I) % 3], axis = 1)
+    allChoices = np.concatenate([(x_mat + mod_I) % 3, (x_mat + 2 * mod_I) % 3], axis=1)
     allChoices_pairs = pairsMat(allChoices, pairs)
 
-    vals = (allChoices_pairs.T.dot(pairsS)*allChoices_pairs.T).sum(axis=1)
+    vals = (allChoices_pairs.T.dot(pairsS) * allChoices_pairs.T).sum(axis=1)
     max_idx = np.argmax(vals)
     max_val = vals[max_idx]
 
@@ -73,37 +74,34 @@ def localMove(pairsS, x, pairs, intitialVal):
     return success, max_val, (allChoices[:, max_idx]).flatten()
 
 
-
 def setStart(m, start, X, B, PB, PX, TB, TX, QB, QX):
     m.params.StartNumber = 0
     d = len(start)
 
-    b_vals = (2*d)*[0]
+    b_vals = (2 * d) * [0]
 
     for i in range(d):
         if start[i] == 1:
-            b_vals[2*i] = 1
+            b_vals[2 * i] = 1
         elif start[i] == 2:
-            b_vals[2*i] = b_vals[2*i + 1] = 1
-
+            b_vals[2 * i] = b_vals[2 * i + 1] = 1
 
     for i in X:
         X[i].Start = start[i]
 
-
-    for i in range(2*d):
+    for i in range(2 * d):
         B[i].Start = b_vals[i]
 
     for i, j in PB:
         PB[i, j].Start = b_vals[i] * b_vals[j]
 
     for i, j in PX:
-        PX[i, j].Start =  start[i] * start[j]
+        PX[i, j].Start = start[i] * start[j]
 
-    for i,j,k in TB:
+    for i, j, k in TB:
         TB[i, j, k].Start = b_vals[i] * b_vals[j] * b_vals[k]
 
-    for i,j,k in TX:
+    for i, j, k in TX:
         TX[i, j, k].Start = start[i] * start[j] * start[k]
 
     for i, j, k, l in QB:
@@ -113,24 +111,20 @@ def setStart(m, start, X, B, PB, PX, TB, TX, QB, QX):
         QX[i, j, k, l].Start = start[i] * start[j] * start[k] * start[l]
 
 
-
-
 def localSearch(pairsS, P):
     local_time = time.perf_counter()
     d = pairsS.shape[0] - len(P)
 
-
     trials = 100
-    Y = np.random.randint(low = 0, high = 3, size = (d, trials))
+    Y = np.random.randint(low=0, high=3, size=(d, trials))
     Y[0, :] = 1
     Y_pairs = pairsMat(Y, P)
 
     # get all y^T pairsS y for all columns y of Y_pairs
-    vals = (Y_pairs.T.dot(pairsS)*Y_pairs.T).sum(axis=1)
+    vals = (Y_pairs.T.dot(pairsS) * Y_pairs.T).sum(axis=1)
     max_idx = np.argmax(vals)
     x = Y[:, max_idx].flatten()
     currVal = vals[max_idx]
-
 
     while True:
         success, val, newVec = localMove(pairsS, x, P, currVal)
@@ -141,7 +135,7 @@ def localSearch(pairsS, P):
         currVal = val
         x = newVec
 
-    #print(f"LS found solution of value {currVal}")
+    # print(f"LS found solution of value {currVal}")
     local_time = time.perf_counter() - local_time
     return currVal, x, local_time
 
@@ -149,34 +143,31 @@ def localSearch(pairsS, P):
 # let S be a set of vectors in (d, k), meaning columns of S are the vectors in the solution
 
 def sparsify(S, W, k):
-    d  = S.shape[0]
+    d = S.shape[0]
     s = S.shape[1]
 
     m = Model()
     m.Params.LogToConsole = 0
 
-
-    newW = m.addVars(s, lb = 0, ub = k)
-    #m.addMVar((s,), lb = 0, ub = k)
+    newW = m.addVars(s, lb=0, ub=k)
+    # m.addMVar((s,), lb = 0, ub = k)
     m.addLConstr(newW.sum() == k)
 
-    #L = S @ np.diag(W) @ S.T
-    #R = S @ np.diag(newW) @ S.T
+    # L = S @ np.diag(W) @ S.T
+    # R = S @ np.diag(newW) @ S.T
 
-    #m.addConstrs(L[i, j] == R[i, j] for i in range(d) for j in range(i, d))
+    # m.addConstrs(L[i, j] == R[i, j] for i in range(d) for j in range(i, d))
 
     for i in range(d):
-        for j in range(i,d):
-            LHS = quicksum([newW[l]*S[i, l]*S[j, l]  for l in range(s)] )
-            RHS = sum([W[l]*S[i, l]*S[j, l]  for l in range(s)] )
+        for j in range(i, d):
+            vals = [S[i, l] * S[j, l] for l in range(s)]
+            LHS = quicksum([newW[l] * vals[l] for l in range(s)])
+            RHS = sum([W[l] * vals[l] for l in range(s)])
             m.addLConstr(LHS == RHS)
-
 
     m.optimize()
 
     return [newW[i].x for i in range(s)] if m.status == GRB.OPTIMAL else []
-
-
 
 
 def powerset(s):
@@ -185,60 +176,55 @@ def powerset(s):
     for i in range(1 << x):
         yield [ss for mask, ss in zip(masks, s) if i & mask]
 
+
 # maximize x^T A x for x binary and A symmetric
 # and first coordinate of x is 1
-def quadIP(S, onesBound = None, pairs = []):
+def quadIP(S, onesBound=None, pairs=[]):
     time_IP = time.perf_counter()
     d = S.shape[0] - len(pairs)
     m = Model()
 
-
-
     ind = [(i, j) for i in range(d) for j in range(i + 1, d)]
     l = tuplelist(ind)
-    X = m.addVars(d, vtype = GRB.BINARY)
+    X = m.addVars(d, vtype=GRB.BINARY)
     #  obj = S.diagonal()
     # pairs
-    P = m.addVars(l, vtype = 'C', lb = 0, ub = 1)
+    P = m.addVars(l, vtype='C', lb=0, ub=1)
 
     T = []
     Q = []
     if len(pairs):
-        T = m.addVars(tuplelist([(i, j, k) for j,k in pairs for i in range(d)]), vtype = 'C', lb = 0, ub  = 1)
-        qind = [(pairs[i][0], pairs[i][1], pairs[j][0], pairs[j][1]) for i in range(len(pairs)) for j in range(i + 1, len(pairs))]
-        Q = m.addVars(tuplelist(qind), vtype = 'C', lb = 0, ub  = 1)
-
-
+        T = m.addVars(tuplelist([(i, j, k) for j, k in pairs for i in range(d)]), vtype='C', lb=0, ub=1)
+        qind = [(pairs[i][0], pairs[i][1], pairs[j][0], pairs[j][1]) for i in range(len(pairs)) for j in
+                range(i + 1, len(pairs))]
+        Q = m.addVars(tuplelist(qind), vtype='C', lb=0, ub=1)
 
     m.addConstr(X[0] == 1)
 
     for i, j in P:
-        prodConstraint(m, X[i], X[j], P[i, j])
+        prod_constraint(m, X[i], X[j], P[i, j])
 
-    for i,j,k in T:
+    for i, j, k in T:
         if i == j or i == k:
             m.addLConstr(T[i, j, k] == P[j, k])
         else:
-            prodConstraint(m, X[i], P[j, k], T[i, j, k])
+            prod_constraint(m, X[i], P[j, k], T[i, j, k])
 
-    for i,j,k,l in Q:
+    for i, j, k, l in Q:
         if i == k:
             m.addLConstr(Q[i, j, k, l] == T[j, k, l])
         elif j == l:
             m.addLConstr(Q[i, j, k, l] == T[i, k, l])
         else:
-            prodConstraint(m, P[i,j], P[k, l], Q[i, j, k, l])
+            prod_constraint(m, P[i, j], P[k, l], Q[i, j, k, l])
 
-
-
-    obj = S.diagonal() @ np.array([X[i] for i in X] + [P[i, j] for i,j in pairs]) + quicksum([2 * P[i, j] *S[i, j] for i,j in P])
-
-
+    obj = S.diagonal() @ np.array([X[i] for i in X] + [P[i, j] for i, j in pairs]) + quicksum(
+        [2 * P[i, j] * S[i, j] for i, j in P])
 
     for i in range(d):
         for idx, t in enumerate(pairs):
             p1, p2 = t
-            obj += T[i, p1, p2] * 2*S[i, idx + d]
+            obj += T[i, p1, p2] * 2 * S[i, idx + d]
 
     for i in range(len(pairs)):
         p11, p12 = pairs[i]
@@ -248,66 +234,60 @@ def quadIP(S, onesBound = None, pairs = []):
 
     m.setObjective(obj, GRB.MAXIMIZE)
 
-
     if onesBound is not None:
         m.addLConstr(X.sum() <= onesBound)
-    #m.ModelSense = -1
+    # m.ModelSense = -1
     m.Params.LogToConsole = 0
     m.optimize()
     time_IP = time.perf_counter() - time_IP
 
     sol = [int(X[j].x) for j in range(d)]
 
-    return m.getObjective().getValue() ,sol, time_IP
+    return m.getObjective().getValue(), sol, time_IP
 
-def quadIP_two(S, pairs, start = None):
+
+def quadIP_two(S, pairs, start=None):
     time_IP = time.perf_counter()
     d = S.shape[0] - len(pairs)
-    #print(f"intially dim is {d}")
+    # print(f"intially dim is {d}")
     m = Model()
 
-
-    X = m.addVars(d, vtype = 'C', lb = 0, ub = 2)
-    B = m.addVars(2*d, vtype = 'B')
+    X = m.addVars(d, vtype='C', lb=0, ub=2)
+    B = m.addVars(2 * d, vtype='B')
 
     m.addConstr(X[0] == 1)
 
-    m.addConstrs((X[i] == B[2*i] + B[2*i + 1] for i in range(d)), name = "xEQ")
-    m.addConstrs((B[2*i] >= B[2*i + 1] for i in range(d)), name = "sym")
+    m.addConstrs((X[i] == B[2 * i] + B[2 * i + 1] for i in range(d)), name="xEQ")
+    m.addConstrs((B[2 * i] >= B[2 * i + 1] for i in range(d)), name="sym")
 
+    PB = m.addVars(tuplelist([(i, j) for i in range(2 * d) for j in range(i + 1, 2 * d)]), vtype='C', lb=0, ub=1)
 
-    PB = m.addVars(tuplelist([(i, j) for i in range(2*d) for j in range(i + 1, 2*d)]), vtype = 'C', lb = 0, ub = 1)
-
-
-    PX = m.addVars(tuplelist([(i, j) for i in range(d) for j in range(i, d)]), vtype = 'C', lb = 0, ub = 4)
+    PX = m.addVars(tuplelist([(i, j) for i in range(d) for j in range(i, d)]), vtype='C', lb=0, ub=4)
 
     for i, j in PB:
-        prodConstraint(m, B[i], B[j], PB[i, j])
+        prod_constraint(m, B[i], B[j], PB[i, j])
 
-
-
-    m.addConstrs(PX[i, j] == PB[2*i, 2*j]  + PB[2*i, 2*j + 1] + PB[2*i + 1, 2*j] + PB[2*i + 1, 2*j + 1] for i,j in PX if i != j)
-    m.addConstrs(PX[i, i] == B[2*i] + B[2*i + 1] + 2*PB[2*i, 2*i + 1] for i in range(d))
-
+    m.addConstrs(
+        PX[i, j] == PB[2 * i, 2 * j] + PB[2 * i, 2 * j + 1] + PB[2 * i + 1, 2 * j] + PB[2 * i + 1, 2 * j + 1] for i, j
+        in PX if i != j)
+    m.addConstrs(PX[i, i] == B[2 * i] + B[2 * i + 1] + 2 * PB[2 * i, 2 * i + 1] for i in range(d))
 
     tripleInd = []
     tripleX = []
     for i in range(d):
         for j, k in pairs:
-            tmp = [(a, b, c) for a in [2*i, 2*i + 1] for b in [2*j, 2*j + 1] for c in [2*k, 2*k + 1]]
+            tmp = [(a, b, c) for a in [2 * i, 2 * i + 1] for b in [2 * j, 2 * j + 1] for c in [2 * k, 2 * k + 1]]
             tripleInd.extend(tmp)
             tripleX.append((i, j, k))
 
+    TB = m.addVars(tuplelist(tripleInd), vtype='C')
+    for i, j, k in TB:
+        prod_constraint(m, B[i], PB[j, k], TB[i, j, k])
 
-    TB = m.addVars(tuplelist(tripleInd), vtype = 'C')
-    for i,j, k in TB:
-        prodConstraint(m, B[i], PB[j, k], TB[i, j, k])
-
-    TX = m.addVars(tuplelist(tripleX), vtype = 'C', lb = 0, ub = 8)
+    TX = m.addVars(tuplelist(tripleX), vtype='C', lb=0, ub=8)
     for i, j, k in TX:
-        RHS = [TB[a, b, c] for a in [2*i, 2*i + 1] for b in [2*j, 2*j + 1] for c in [2*k, 2*k + 1]]
+        RHS = [TB[a, b, c] for a in [2 * i, 2 * i + 1] for b in [2 * j, 2 * j + 1] for c in [2 * k, 2 * k + 1]]
         m.addLConstr(TX[i, j, k] == quicksum(RHS))
-
 
     quadInd = []
     quadX = []
@@ -315,32 +295,33 @@ def quadIP_two(S, pairs, start = None):
         t1, t2 = pairs[i]
         for j in range(i, len(pairs)):
             s1, s2 = pairs[j]
-            tmp = [(a, b, c, e)  for a in [2*t1, 2*t1 + 1] for b in [2*t2, 2*t2 + 1] for c in [2*s1, 2*s1 + 1] for e in [2*s2, 2*s2 + 1]]
+            tmp = [(a, b, c, e) for a in [2 * t1, 2 * t1 + 1] for b in [2 * t2, 2 * t2 + 1] for c in
+                   [2 * s1, 2 * s1 + 1] for e in [2 * s2, 2 * s2 + 1]]
             quadInd.extend(tmp)
             quadX.append((t1, t2, s1, s2))
 
-    QB = m.addVars(tuplelist(quadInd), vtype = 'C')
-
+    QB = m.addVars(tuplelist(quadInd), vtype='C')
 
     for a, b, c, e in QB:
-        prodConstraint(m, PB[a, b], PB[c, e], QB[a, b, c, e])
+        prod_constraint(m, PB[a, b], PB[c, e], QB[a, b, c, e])
 
-
-    QX = m.addVars(tuplelist(quadX), vtype = 'C', lb = 0, ub = 16)
+    QX = m.addVars(tuplelist(quadX), vtype='C', lb=0, ub=16)
     for t1, t2, s1, s2 in quadX:
-        RHS = [QB[a, b, c, e]  for a in [2*t1, 2*t1 + 1] for b in [2*t2, 2*t2 + 1] for c in [2*s1, 2*s1 + 1] for e in [2*s2, 2*s2 + 1]]
+        RHS = [QB[a, b, c, e] for a in [2 * t1, 2 * t1 + 1] for b in [2 * t2, 2 * t2 + 1] for c in [2 * s1, 2 * s1 + 1]
+               for e in [2 * s2, 2 * s2 + 1]]
         m.addLConstr(QX[t1, t2, s1, s2] == quicksum(RHS))
 
-    #start = localSearch(S, pairs)
+    # start = localSearch(S, pairs)
     if start is not None:
         setStart(m, start, X, B, PB, PX, TB, TX, QB, QX)
 
-    obj = S.diagonal() @ np.array([PX[i, i] for i in range(d)] + [QX[i, j, i, j] for i,j in pairs]) + quicksum([2*PX[i, j]*S[i, j] for i,j in PX if i != j])
+    obj = S.diagonal() @ np.array([PX[i, i] for i in range(d)] + [QX[i, j, i, j] for i, j in pairs]) + quicksum(
+        [2 * PX[i, j] * S[i, j] for i, j in PX if i != j])
 
     for i in range(d):
         for idx, t in enumerate(pairs):
             p1, p2 = t
-            obj += TX[i, p1, p2] * 2*S[i, idx + d]
+            obj += TX[i, p1, p2] * 2 * S[i, idx + d]
 
     for i in range(len(pairs)):
         p11, p12 = pairs[i]
@@ -352,7 +333,6 @@ def quadIP_two(S, pairs, start = None):
 
     m.Params.LogToConsole = 0
     m.optimize()
-    time_IP = time.perf_counter() - time_IP
     sol = [X[j].x for j in range(d)]
 
-    return m.getObjective().getValue() ,sol, time_IP
+    return m.getObjective().getValue(), sol, time.perf_counter() - time_IP
