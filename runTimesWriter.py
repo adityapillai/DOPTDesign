58e0f95babc13bc469b633d9bd3a65d6f0abb306
Aditya Pillai <aditya.pillai53916@gmail.com>
2024-01-28 17:44:49 -0500

    cleaned up and added functinos to simplify rrunTimesWriter.py

--- runTimesWriter.py
+++ runTimesWriter.py
@@ -1,33 +1,34 @@
 import pandas as pd
 import numpy as np
 import CGZ
+import localZ
+from functools import partial
 
 
+def ones_alg(d_vals, k_vals, q_vals, local, solver):
+    func = localZ.local_alg if local else partial(CGZ.colgen_ones_bound, solver=solver)
+    dics = [func(d=d, k=k, ones_bound=q) for d, k, q in zip(d_vals, k_vals, q_vals)]
+    return pd.DataFrame(dics)
 
 
+def pairs_alg(d_vals, k_vals, pairs, local, solver):
+    fun = localZ.local_alg_pairs if local else partial(CGZ.colgen_pairs, solver=solver)
+    dics = [fun(d=d, k=k, pairs=pairs) for d, k in zip(d_vals, k_vals)]
+    return pd.DataFrame(dics)
 
-
-
-
-primalSolver = "knitro"
 
 d_ranges = np.arange(25, 41)
-k_ranges = 2*d_ranges
-p_ranges = d_ranges//3
+k_ranges = 2 * d_ranges
+p_ranges = d_ranges // 3
 
-Pairs = [(1, 3) ,(4, 5), (6, 14), (8, 17)]
+Pairs = [(1, 3), (4, 5), (6, 14), (8, 17)]
 
-# experiment 1 for runtimes of first variant
-#variant = "Ones_Bound"
-#dics = [CGZ.colgen_p(d, k, p, solver = primalSolver) for d,k,p in zip(d_ranges, k_ranges, p_ranges)]
-variant = "pairs"
-dics = [CGZ.colgen_pairs(d,k, Pairs, solver = primalSolver) for d,k in zip(d_ranges, k_ranges)]
+df_ones = ones_alg(d_ranges, k_ranges, p_ranges, False, solver="Knitro")
+df_ones.to_csv("Data/Ones-CG-v5.csv", index=False)
+df_ones_local = ones_alg(d_ranges, k_ranges, p_ranges, True, solver="Knitro")
+df_ones_local.to_csv("Data/Ones-LS-v5.csv", index=False)
 
-
-df = pd.DataFrame(dics)
-df.set_index("F/s")
-col_order = ["F/s", "Pairs", "Total Iterations", "Total Time", "Gurobi Time", f"{primalSolver} Time", "Mosek Time", "Mosek Iterations", "Times Sparsified", "Sprase Time", "CP Value"]
-# reorder columns for better readbility in CSV
-df = df[col_order]
-
-df.to_csv(f"{variant}-{primalSolver}.csv", index = False)
+df_pairs = pairs_alg(d_ranges, k_ranges, Pairs, False, solver="Knitro")
+df_pairs.to_csv("Data/Pairs-CG-v5.csv", index=False)
+df_pairs_loc = pairs_alg(d_ranges, k_ranges, Pairs, True, solver="Knitro")
+df_pairs_loc.to_csv("Data/Pairs-LS-v5.csv", index=False)
